a43fa25f4c9b55fd6e2983c84924ff110e26fb43
nothendev <borodinov.ilya@gmail.com>
2023-04-10 10:36:15 +0300

    fix edit for index

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,5 +2,8 @@ const REPO = 'makeplane/docs'
 const BRANCH = 'master'
 
 export function githubEdit(file) {
+  if (file.startsWith('/')) {
+    file = 'index.mdx'
+  }
   return `https://github.com/${REPO}/edit/${BRANCH}/src/pages/${file}`
 }
