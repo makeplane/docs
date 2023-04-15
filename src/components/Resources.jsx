c3ef28a9915624a03bc024fa6688cf1c115b3bb4
Vihar Kurama <viharkurama@Vihars-MacBook-Pro.local>
2023-04-15 13:30:16 +0530

    update introduction

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -143,7 +143,7 @@ export function Resources() {
   return (
     <div className="my-16 xl:max-w-none">
       <Heading level={2} id="resources">
-        Resources
+        Explore Plane
       </Heading>
       <div className="not-prose mt-4 grid grid-cols-1 gap-8 border-t border-zinc-900/5 pt-10 dark:border-white/5 sm:grid-cols-2 xl:grid-cols-4">
         {resources.map((resource) => (
