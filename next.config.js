aafd7d0567f35b287bf86e0f74f924d0a4c62ee8
Patrick Black <info@patrickblackjr.com>
2023-05-25 18:57:45 -0500

    build: add `tsx` and `ts` to next and tailwind

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withMarkdoc = require('@markdoc/next.js')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'md'],
+  pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
   experimental: {
     scrollRestoration: true,
   },
