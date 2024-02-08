b01006931a75c5308fe7ee214a1b4d9c093cca65
Ramesh Kumar Chandra <rameshkumar2299@gmail.com>
2024-02-08 18:58:32 +0530

    fix/self hosting page 404 issue on url launch

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@ const withMarkdoc = require('@markdoc/next.js')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/self-hosting',
-        destination: '/self-hosting/docker-compose',
-        permanent: true,
-      },
-    ]
-  },
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md'],
   experimental: {
