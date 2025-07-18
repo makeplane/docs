d0bc2f1a4ace7c21cee847a91c747ab26caae9af
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2025-07-18 16:19:06 +0530

    chore: upgrade docusaurus to 3.8

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,6 +2,8 @@ import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
 const config: Config = {
   title: "Plane",
   tagline: "An easy, flexible project management software",
@@ -9,6 +11,10 @@ const config: Config = {
   trailingSlash: false,
   // Set the production url of your site here
   url: "https://docs.plane.so",
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -16,7 +22,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "makeplane", // Usually your GitHub org/user name.
   projectName: "docs", // Usually your repo name.
-
+  // Broken links
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   onBrokenAnchors: "warn",
@@ -38,11 +44,10 @@ const config: Config = {
           routeBasePath: "/",
           sidebarPath: "./sidebars.ts",
           include: ["**/*.md", "**/*.mdx"],
-          //sidebarCollapsible: false,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          //editUrl:
-          // 'https://github.com/makeplane/docs/edit/preview/',
+        },
+        blog: false,
+        theme: {
+          customCss: "./src/css/custom.css",
         },
         sitemap: {
           lastmod: "date",
@@ -50,10 +55,6 @@ const config: Config = {
           priority: 0.5,
           ignorePatterns: ["/tags/**"],
           filename: "sitemap.xml",
-        },
-        blog: false,
-        theme: {
-          customCss: "./src/css/custom.css",
         },
         gtag: {
           trackingID: "G-G578SD4VZD",
@@ -63,43 +64,14 @@ const config: Config = {
     ],
   ],
 
-  stylesheets: [
-    {
-      href: "https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=National+Park:wght@200..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap",
-      type: "text/css",
-    },
-  ],
-
-  scripts: [
-    {
-      src: "https://plausible.io/js/script.js",
-      defer: true,
-      "data-domain": "docs.plane.so",
-    },
-    {
-      src: "https://cdn.cr-relay.com/v1/site/b1fcbcbd-67f6-4736-940f-033731801664/signals.js",
-      async: true,
-    },
-  ],
-
-  plugins: [
-    [
-      "posthog-docusaurus",
-      {
-        apiKey: "phc_HskAKGRy7x0BEoWfyeHzYWWzcMmKG9DCail7ot7WQkA",
-        appUrl: "https://us.i.posthog.com", // optional
-        enableInDevelopment: false, // optional
-      },
-    ],
-    [
-      "@docusaurus/plugin-google-tag-manager",
-      {
-        containerId: "G-G578SD4VZD",
-      },
-    ],
-  ],
-
   themeConfig: {
+    image: "https://media.docs.plane.so/logo.svg",
+    docs: {
+      sidebar: {
+        hideable: false,
+        autoCollapseCategories: false,
+      },
+    },
     metadata: [
       {
         property: "og:image",
@@ -119,14 +91,6 @@ const config: Config = {
       disableSwitch: false,
       respectPrefersColorScheme: false,
     },
-    docs: {
-      sidebar: {
-        hideable: false,
-        autoCollapseCategories: false,
-      },
-    },
-    // Replace with your project's social card
-    image: "https://media.docs.plane.so/logo.svg",
     algolia: {
       appId: "AXICJJC8RP",
       apiKey: "23df4157dee1d9a8d435cadd6cae3f26",
@@ -163,56 +127,41 @@ const config: Config = {
         },
       ],
     },
-    // footer: {
-    //   style: 'dark',
-    //   links: [
-    //     {
-    //       title: 'Docs',
-    //       items: [
-    //         {
-    //           label: 'Tutorial',
-    //           to: '/docs/intro',
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       title: 'Community',
-    //       items: [
-    //         {
-    //           label: 'Stack Overflow',
-    //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-    //         },
-    //         {
-    //           label: 'Discord',
-    //           href: 'https://discordapp.com/invite/docusaurus',
-    //         },
-    //         {
-    //           label: 'Twitter',
-    //           href: 'https://twitter.com/docusaurus',
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       title: 'More',
-    //       items: [
-    //         {
-    //           label: 'Blog',
-    //           to: '/blog',
-    //         },
-    //         {
-    //           label: 'GitHub',
-    //           href: 'https://github.com/facebook/docusaurus',
-    //         },
-    //       ],
-    //     },
-    //   ],
-    //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    // },
+    footer: {},
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+
+  scripts: [
+    {
+      src: "https://plausible.io/js/script.js",
+      defer: true,
+      "data-domain": "docs.plane.so",
+    },
+    {
+      src: "https://cdn.cr-relay.com/v1/site/b1fcbcbd-67f6-4736-940f-033731801664/signals.js",
+      async: true,
+    },
+  ],
+
+  plugins: [
+    [
+      "posthog-docusaurus",
+      {
+        apiKey: "phc_HskAKGRy7x0BEoWfyeHzYWWzcMmKG9DCail7ot7WQkA",
+        appUrl: "https://us.i.posthog.com", // optional
+        enableInDevelopment: false, // optional
+      },
+    ],
+    [
+      "@docusaurus/plugin-google-tag-manager",
+      {
+        containerId: "G-G578SD4VZD",
+      },
+    ],
+  ],
 };
 
 export default config;
