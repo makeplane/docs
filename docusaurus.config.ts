5853054e4c8dba509d19f81a8ea3430a59c79240
Dancia <claradancia20@gmail.com>
2025-10-11 20:32:53 +0530

    New sidebar structure (#358)
    * New sidebar structure
    
    * Structured the sidebar
    
    * code rabbit fixes
    
    * code rabbit fixes
    
    * Updated workspace and members pages
    
    * Updated billing and account sections
    
    * fixed broken links
    
    * updated more pages
    
    * fixed broken linke
    
    * Updated image CSS and fixed images
    
    * Fixed images
    
    * minor fix
    
    * code rabbit fixes
    
    * changed tool to software
    
    * minor fix
    
    * minor fix
    
    * added fallback css for images
    
    * removed fallback css for images

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
   title: "Plane",
-  tagline: "A modern project management tool",
+  tagline: "Modern project management software",
   favicon: "https://media.docs.plane.so/logo/favicon-32x32.png",
   trailingSlash: false,
   // Set the production url of your site here
